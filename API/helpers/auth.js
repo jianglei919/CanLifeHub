d9148a1b2111772870ae862afaab72c407cc89d1
jianglei <990864781@qq.com>
2025-11-06 17:30:53 -0500

    rebuild whole project struct

---
+++ API/helpers/auth.js
@@ -0,0 +1,27 @@
+// bcrypt 用于密码哈希
+const bcrypt = require('bcrypt');
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(12, (err, salt) => {
+            if (err) {
+                reject(err);
+            }
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(hash);
+            })
+        })
+    })
+}
+
+const comparePassword = (password, hashed) => {
+    return bcrypt.compare(password, hashed);
+}
+
+exports = module.exports = {
+    hashPassword,
+    comparePassword
+}
